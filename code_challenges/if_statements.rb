590f003202b3ace61950fd2c28dde57c80520e84
Jorja Fleming <aset284@gmail.com>
2023-02-23 13:44:25 -0700

    Add if_statements assignment

---
+++ code_challenges/if_statements.rb
@@ -0,0 +1,43 @@
+weather = "sunny"
+
+if weather == "sunny"
+  p "sunscreen"
+elsif weather == "rainy"
+  p "umbrella"
+elsif weather == "snowy"
+  p "coat"
+elsif weather == "icy"
+  p "yak traks"
+else
+  p "good to go!"
+end
+
+weather = "icy"
+
+if weather == "sunny"
+  p "sunscreen"
+elsif weather == "rainy"
+  p "umbrella"
+elsif weather == "snowy"
+  p "coat"
+elsif weather == "icy"
+  p "yak traks"
+else
+  p "good to go!"
+end
+
+num_quarters = 0
+
+if num_quarters >= 2
+puts "I have enough money for a gumball"
+elsif
+puts "I don't have enough money for a gumball"
+end
+
+cups_of_flour = 2
+has_sauce = true
+
+If cups_of_flour = 1 and has_sauce = true, print "I cannot make pizza"
+If cups_of_flour = 5 and has_sauce = false, print "I cannot make pizza"
+If cups_of_flour = 2 and has_sauce = true, print "I can make pizza"
+If cups_of_flour = 3 and has_sauce = true, print "I can make pizza"
